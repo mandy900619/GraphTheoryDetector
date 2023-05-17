7cd151654e613004291211112191676da8ce523c
Y3NH0 <edelick85219913@gmail.com>
2023-05-17 15:25:10 +0800

    update env setup script

--- Makefile
+++ Makefile
@@ -1,9 +1,8 @@
-all:	models env
+all: models env
 
 models:
 	unzip -d ./FC_Model/ FC_Model/other_FC_model.zip
 	unzip -d ./MD_Model/ MD_Model/other_MD_model.zip
 
 env:
-	conda create -y -n GraphTheoryDetector
 	./env.sh
--- env.sh
+++ env.sh
@@ -3,15 +3,24 @@
 install_requirements() {
     file="$1"
 
-    while read -r line; do
+    while read -r line || [[ -n $line ]]; do
+        echo "INSTALL $line"
+
         conda install -y $line
 
         # $? == return val of "conda install", successfully installed if $? == 0
         if [ $? -ne 0 ]; then
+            echo "c->p"
             # use pip if conda doesnt work
             pip install $line
         fi
     done < "$file"
 }
 
+# create conda env & install package
+env_name="GraphTheoryDetector"
+conda create -y -n $env_name
+source ~/.bashrc
+source activate
+conda activate $env_name
 install_requirements requirements.txt
